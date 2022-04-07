bed4ba0d828730098136bacc5f783847c84b3efe
josdugan <josdugan@gmail.com>
2022-04-07 16:18:42 -0400

    Add pug templates

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ const PORT = 3000;
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 const server = app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
+});
+
+app.get('/', (req, res, next) => {
+  const payload = {
+    pageTitle: 'Home',
+  };
+
+  res.status(200).render('home', payload);
 });
