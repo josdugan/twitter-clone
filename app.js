0baf5c7eeb54bbb155aba0b99aaf884a03282ead
josdugan <josephdug@gmail.com>
2023-04-30 13:01:16 -0400

    Create login page.

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
+const path = require("path");
 const express = require("express");
+const middleware = require("./middleware");
+const loginRoute = require("./routes/loginRoutes");
 
 const PORT = 3000;
 
@@ -6,8 +9,23 @@ const app = express();
 
 app.set("view engine", "pug");
 app.set("views", "views");
+app.use(
+  "/css",
+  express.static(path.join(__dirname, "node_modules/bootstrap/dist/css"))
+);
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "node_modules/bootstrap/dist/js"))
+);
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "node_modules/jquery/dist"))
+);
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res, next) => {
+app.use("/login", loginRoute);
+
+app.get("/", middleware.requireLogin, (req, res, next) => {
   const payload = {
     pageTitle: "Home",
   };
---
+++ middleware.js
@@ -0,0 +1,10 @@
+const requireLogin = (req, res, next) => {
+  if (req.session && req.session.user) {
+    return next();
+  }
+  return res.redirect("/login");
+};
+
+module.exports = {
+  requireLogin,
+};
